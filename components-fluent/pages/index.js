4ab8013ff5e4d5f4d498edf3c49cd1df340d267f
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-08-10 12:58:31 -0500

    updated client and herobanner

--- components-fluent/pages/index.js
+++ components-fluent/pages/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
-
+import { product, FooterBanner, HeroBanner } from '../components';
+import { client }from '../lib/client';
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner />
 
-      <div>
+      <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Computer Parts That Are Available</p>
       </div>
 
-      <div>
+      <div className='products-container'>
         {[ 'Product1', 'product2' ].map(
           (product) => product)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   )
 }
